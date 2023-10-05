9d28accc3b749c40fc5fcd5245b17da729cfd3dd
Divkov575 <dmitriy@ivkov.net>
2023-10-05 12:52:55 -0400

    adding clap commands

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,31 @@
-use anyhow::{Result};
-use clap;
 mod config_parsing;
 mod config;
+mod commands;
+
+use anyhow::{Result};
+use clap;
+use commands::Command;
+
+
+#[derive(Debug, Parser)]
+#[command(
+    version,
+    about,
+    long_about = None,
+    after_help = "Note: `trash -h` prints a short and concise overview while `trash --help` gives all \
+                 details.",
+)]
+pub struct Args {
+    /// The command to run.
+    #[clap(subcommand)]
+    pub command: Option<Command>,
+
+    // #[clap(flatten)]
+    // config_args: ConfigArgs,
+
+    // #[clap(flatten)]
+    // put_args: put::PutArgs,
+}
 
 
 fn main() -> Result<()> {
