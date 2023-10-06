bb897d70c0e13429e320839a78db0e9783efc771
Divkov575 <dmitriy@ivkov.net>
2023-10-06 12:33:06 -0400

    created command skeleton

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ inspired by matano, executes completely within gcp
 ---
 execute cli command from outside cargo dir \n
 cargo run --manifest-path cli/Cargo.toml
+---
+cli (create config, read config, find terraform/gcp -> upload stack, upload code)
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,10 +2,24 @@
 // mod config;
 mod commands;
 
+use std::process::Command;
 use anyhow::Result;
 use clap::{self, Parser};
 // use exitcode::ExitCode;
 
+// fn terraform_init() -> Result<()> {
+    // let output = Command::new("terraform").arg("init").output()?;
+//     print!("{:#?}", output);
+//     // print!("{:#?}", output);
+//     Ok(())
+// }
+
+// fn terraform_plan() -> Result<()> {
+//     let output = Command::new("terraform").arg("").output()?;
+//     print!("{:#?}", output);
+//     // print!("{:#?}", output);
+//     Ok(())
+// }
 
 
 fn main() -> Result<()> {
@@ -13,6 +27,9 @@ fn main() -> Result<()> {
     //     Ok(()) => ExitCode::Success.exit(),
     //     Err(e) => ExitCode::Error.exit_with_msg(format!("{e:#}")),
     // }
+
+    // print!("{:#?}", Command::new("terraform").output()?);
+    
 
     commands::Args::parse().run()?;
     Ok(())
