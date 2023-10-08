a644a30c1c90691b90420e6f61171473d130913c
Divkov575 <dmitriy@ivkov.net>
2023-10-07 20:52:33 -0400

    adding previously unstaged cli skeleton files + terraform stuff

---
+++ Infra/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "project" {
+  type = string
+  default = "neon-circle-400322"
+}
+
+variable "container_image1" {
+  type = string
+  default = "timberio/vector:0.33.0-debian"
+}
+
+
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,6 +4,7 @@ use clap::{self, Parser};
 
 
 
+
 fn main() -> Result<()> {
     commands::Args::parse().run()?;
     Ok(())
