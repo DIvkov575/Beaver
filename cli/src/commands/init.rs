8dae30b881eec74b736cf9eb3959172291cea4b5
Divkov575 <dmitriy@ivkov.net>
2024-01-02 17:54:05 -0500

    lots of progress on crj + began gcs.rs + working on cli

--- cli/src/commands/init.rs
+++ cli/src/commands/init.rs
@@ -21,20 +21,21 @@ pub fn create_config_dir(file_path: &str) -> Result<()> {
 pub fn init() -> Result<()> {
     let regions: Vec<&str> = vec![ "northamerica-northeast1", "us-west4", "southamerica-east1", "australia-southeast1", "asia-southeast2", "australia-southeast2", "asia-south1", "asia-northeast2", "australia-east", "asia-east2", "europe-north1", "asia-northeast1", "asia-east1", "europe-west2", "us-central1", "europe-west1", "us-east1", "us-east4", "southamerica-west1", "us-west2", "asia-south2", "europe-west6", "asia-southeast1", "europe-west4", "europe-north2", "europe-west3", "us-west1", "us-west3", "europe-west5", "australia-central2"];
 
-    println!("---- Beaver: Setup Wizard ----");
+    println!("\n---- Beaver: Setup Wizard ----");
     let region = Select::new("Select GCP Region:", regions).prompt()?;
     let project = &Text::new("Enter GCP project-id:").prompt()?;
+    let config_path = &Text::new("Enter path to config directory:").prompt()?;
 
-    if !Confirm::new("Do you have an existing Beaver Directory (y/n)").prompt()? {
-        let _name= Text::new("Config Directory Path:").prompt()?;
-        let path = Path::new(&std::env::current_dir()?).join(&_name);
-        std::fs::create_dir_all(&path)?;
-        create_config_dir(path.to_str().unwrap())?;
-    }
-    
-    let mut spinner = Spinner::new(spinners::Dots, "Initializing Terraform...", Color::Blue); 
-        Command::new("terraform").arg("init").output()?;
-    spinner.success("Terraform Initialized");
+    // if !Confirm::new("Do you have an existing Beaver Directory (y/n)").prompt()? {
+    //     let _name= Text::new("Config Directory Path:").prompt()?;
+    //     let path = Path::new(&std::env::current_dir()?).join(&_name);
+    //     std::fs::create_dir_all(&path)?;
+    //     create_config_dir(path.to_str().unwrap())?;
+    // }
+
+    // let mut spinner = Spinner::new(spinners::Dots, "Initializing Terraform...", Color::Blue);
+    //     Command::new("terraform").arg("init").output()?;
+    // spinner.success("Terraform Initialized");
 
 
 
--- infra/src/crj.rs
+++ infra/src/crj.rs
@@ -1,52 +1,67 @@
 use std::collections::HashMap;
 use std::error::Error;
-use std::process::{Command, Output, Stdio};
+use std::process::{Command, Output};
+
 use serde_yaml::Mapping;
+
 use crate::config::Config;
 
-
-pub fn delete_crj(job_name: &str, config: &Config) -> Result<(), Box<dyn Error>>{
-    let mut args: Vec<&str> =  Vec::from(["run", "jobs", "delete", job_name]);
-    args.extend(config.flatten());
-
-    Command::new("gcloud").args(args).status()?;
-    Ok(())
+pub struct CRJ<'a> {
+    config: &'a Config<'a>,
+    job_name: &'a str,
 }
-pub fn create_crj(job_name: &str, config: &Config) -> Result<(), Box<dyn Error>>{
-    let image_url = "docker.io/timberio/vector:latest-alpine";
-    let mut args: Vec<&str> =  Vec::from(["run", "jobs", "create", job_name, "--image", image_url]);
-    args.extend(config.flatten());
+impl<'a> CRJ<'a> {
+    pub fn new(job_name: &'a str, config: &'a Config) -> Self  {
+        Self {job_name, config}
+    }
+    pub fn delete(job_name: &str, config: &Config) -> Result<(), Box<dyn Error>>{
+        let mut args: Vec<&str> =  Vec::from(["run", "jobs", "delete", job_name]);
+        args.extend(config.flatten());
 
-    Command::new("gcloud").args(args).status()?;
-    Ok(())
-}
-
-pub fn describe_formatted_crj(job_name: &str, config: &Config) -> Result<Vec<u8>, Box<dyn Error>> {
-    let mut args: Vec<&str> =  Vec::from(["run", "jobs", "describe", job_name, "--format", "export"]);
-    args.extend(config.flatten());
-
-    let Output {status: _, stdout: raw_out, stderr: raw_err } = Command::new("gcloud").args(args).output()?;
-    if !raw_err.is_empty() {
-        return Err(String::from_utf8(raw_err)?.into())
+        Command::new("gcloud").args(args).status()?;
+        Ok(())
     }
 
-    Ok(raw_out)
-}
+    pub fn create(&self) -> Result<(), Box<dyn Error>>{
+        self.create_on_gcs()?;
+        self.mount_gcs("", "vector.yaml")?;
+
+        Ok(())
+    }
 
 
-pub fn mount_gcs_crj(job_name: &str, config: &Config) -> Result<(), Box<dyn Error>> {
-    // update crj_description to have gcs mounting point
-    #[inline(always)]
-    fn update_crj_config_file(volume_name: &str, bucket_name: &str, input_description: &mut Mapping ) -> Result<(), Box<dyn Error>> {
-        // teehee visitor pattern?!?!
-        // https://cloud.google.com/run/docs/configuring/jobs/cloud-storage-volume-mounts
+    pub fn create_on_gcs(&self) -> Result<(), Box<dyn Error>>{
+        let image_url = "docker.io/timberio/vector:latest-alpine";
+        let mut args: Vec<&str> =  Vec::from(["run", "jobs", "create", self.job_name, "--image", image_url]);
+        args.extend(self.config.flatten());
 
-        let mount_path = "/etc/vector";
-        let volume_name = "vector.yaml";
+        Command::new("gcloud").args(args).status()?;
+        Ok(())
+    }
+
+    pub fn describe_formatted(&self) -> Result<Vec<u8>, Box<dyn Error>> {
+        let mut args: Vec<&str> =  Vec::from(["run", "jobs", "describe", self.job_name, "--format", "export"]);
+        args.extend(self.config.flatten());
+
+        let Output {status: _, stdout: raw_out, stderr: raw_err } = Command::new("gcloud").args(args).output()?;
+        if !raw_err.is_empty() {
+            return Err(String::from_utf8(raw_err)?.into())
+        }
+
+        Ok(raw_out)
+    }
+
+
+    pub fn mount_gcs(&self, bucket_name: &str, volume_name: &str) -> Result<(), Box<dyn Error>> {
+        #[macro_export]
+        macro_rules! gm {($a:ident, $($b:literal,)*) => {$a$(.get_mut($b).ok_or("error during description unwrap")?)*};}
+
+        let mut description: Mapping = serde_yaml::from_slice(&self.describe_formatted()?)?;
+        // let mut ref_description = &mut description;
 
         let volume_mounts=  Vec::from([
             Mapping::from_iter(HashMap::from([
-                ("mountPath".into(), mount_path.into()),
+                ("mountPath".into(), "/etc/vector".into()),
                 ("name".into(), volume_name.into())
             ]))
         ]);
@@ -64,34 +79,18 @@ pub fn mount_gcs_crj(job_name: &str, config: &Config) -> Result<(), Box<dyn Erro
         ]);
 
 
-        let mut point_a=  gm!(input_description, "spec", "template", "spec", "template", "spec", "containers", 0,).as_mapping_mut().unwrap();
+        let point_a=  gm!(description, "spec", "template", "spec", "template", "spec", "containers", 0,).as_mapping_mut().unwrap();
         point_a.insert("volumeMounts".into(), volume_mounts.into());
-
-        let mut point_b =  gm!(input_description, "spec", "template", "spec", "template", "spec",).as_mapping_mut().unwrap();
+        let point_b =  gm!(description, "spec", "template", "spec", "template", "spec",).as_mapping_mut().unwrap();
         point_b.insert("volumes".into(), volumes.into());
 
-        #[macro_export]
-        macro_rules! gm {
-        ($a:ident, $($b:literal,)*) => {
-           $a$(.get_mut($b).ok_or("error during description unwrap")?)*
-        };
-    }
+        let tmp_file = tempfile::NamedTempFile::new()?;
+        serde_yaml::to_writer(&tmp_file, &description).unwrap();
 
-
+        let mut args: Vec<&str> =  Vec::from(["beta", "run", "jobs", "replace", tmp_file.path().to_str().unwrap()]);
+        args.extend(self.config.flatten());
+        Command::new("gcloud").args(args).status()?;
 
         Ok(())
     }
-
-    let mut description = serde_yaml::from_slice(&describe_formatted_crj(job_name, &config)?)?;
-    update_crj_config_file("", "", &mut description)?;
-
-    let tmp_file = tempfile::NamedTempFile::new()?;
-    serde_yaml::to_writer(tmp_file, &description).unwrap();
-
-    let mut args: Vec<&str> =  Vec::from(["beta", "run", "jobs", "replace", tmp_file.path()]);
-    args.extend(config.flatten());
-    Command::new("gcloud").args(args).status()?;
-
-    Ok(())
 }
-
