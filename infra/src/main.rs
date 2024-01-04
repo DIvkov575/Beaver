8fd721d0ffc9876cd9b670f574ff9a3c9ab9142d
Divkov575 <dmitriy@ivkov.net>
2024-01-03 23:07:52 -0500

    test intlij cmdk pt2

--- infra/src/main.rs
+++ infra/src/main.rs
@@ -33,7 +33,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 
 pub fn check_for_python() -> Result<(String, String)> {
-    //test
     //!  - Inner line doc
     ///  - Outer line doc (exactly 3 slashes)
     /// Output Result< python command, pip command >
