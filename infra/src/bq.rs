2e6caf76b0aa8c8cb16241711020603748cb3f96
Divkov575 <dmitriy@ivkov.net>
2024-01-04 23:46:16 -0500

    added bq, added pubsub (need fixing tho)

--- infra/src/bq.rs
+++ infra/src/bq.rs
@@ -1,12 +1,97 @@
+use std::fmt::format;
 use std::process::Command;
 use anyhow::Result;
-pub fn create_bq() -> Result<()> {
+use crate::config::Config;
+
+pub fn create_table(table_name: &str,dataset_name: &str, config: &Config) -> Result<()> {
+    let id_binding = format!("{}:{}.{}", config.project, dataset_name, table_name);
+    let args: Vec<&str> = Vec::from([
+        "mk",
+        "--table",
+        id_binding.as_ref(),
+        "data: JSON"
+    ]);
+    Command::new("bq").args(args).spawn()?;
+    Ok(())
+}
+
+pub fn create_dataset(dataset_name: &str, config: &Config) -> Result<()> {
     // roles/bigquery.dataEditor
     // roles/bigquery.dataOwner
     // roles/bigquery.user
     // roles/bigquery.admin
 
+    // bq --location=LOCATION mk \
+    // --dataset \
+    // --default_kms_key=KMS_KEY_NAME \
+    // --default_partition_expiration=PARTITION_EXPIRATION \
+    // --default_table_expiration=TABLE_EXPIRATION \
+    // --description="DESCRIPTION" \
+    // --label=LABEL_1:VALUE_1 \
+    // --label=LABEL_2:VALUE_2 \
+    // --max_time_travel_hours=HOURS \
+    // --storage_billing_model=BILLING_MODEL \
+    // PROJECT_ID:DATASET_ID
 
+    let id_binding = format!("{}:{}", config.project, dataset_name);
+    let args: Vec<&str> = Vec::from([
+        "mk",
+        "--dataset",
+        id_binding.as_ref(),
+    ]);
 
+    Command::new("bq").args(args).spawn()?;
     Ok(())
+}
+
+
+pub fn check_for_bq() -> Result<()> {
+    match Command::new("bq").output() {
+        Ok(_) => return Ok(()),
+        Err(_) => panic!("Please ensure you have bq (biqquery utility tool installed)"),
+    }
+}
+
+
+
+pub fn check_for_python() -> Result<(String, String)> {
+    //!  - Inner line doc
+    ///  - Outer line doc (exactly 3 slashes)
+    /// Output Result< python command, pip command >
+
+    let py_version: Option<String>;
+    let py3_version: Option<String>;
+    let pip_version: Option<String>;
+    let pip3_version: Option<String>;
+
+    let mut py_command;
+    let mut pip_command;
+
+    match Command::new("python").arg("--version").output() {
+        Ok(output) => py_version = Some(String::from_utf8(output.stdout)?),
+        Err(_) => py_version = None,
+    }
+    match Command::new("python3").arg("--version").output() {
+        Ok(output) => py3_version = Some(String::from_utf8(output.stdout)?),
+        Err(_) => py3_version = None,
+    }
+    match Command::new("pip").arg("--version").output() {
+        Ok(output) => pip_version = Some(String::from_utf8(output.stdout)?),
+        Err(_) => pip_version = None,
+    }
+    match Command::new("pip3").arg("--version").output() {
+        Ok(output) => pip3_version = Some(String::from_utf8(output.stdout)?),
+        Err(_) => pip3_version = None,
+    }
+
+    if py_version.is_none() && py3_version.is_none() { panic!("Please have python installed on your system as `python` or `python3`"); }
+    if pip_version.is_none() && pip3_version.is_none() { panic!("Please have pip installed on your system as `pip` or `pip3`"); }
+
+    if py3_version.is_some() { py_command = "python3".to_string(); }
+    else { py_command = "python".to_string()}
+
+    if pip3_version.is_some() { pip_command = "pip3".to_string(); }
+    else { pip_command = "pip".to_string()}
+
+    Ok((py_command, pip_command))
 }
--- infra/src/main.rs
+++ infra/src/main.rs
@@ -3,6 +3,7 @@ mod crj;
 mod config;
 mod gcs;
 mod bq;
+mod pubsub;
 
 use anyhow::Result;
 use std::collections::HashMap;
@@ -16,60 +17,26 @@ use serde_yaml;
 use serde_yaml::Mapping;
 use crate::bq::*;
 use crate::gcs::Bucket;
+use crate::pubsub::create_pubsub_topic;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let config = Config::new("us-east1", "neon-circle-400322");
-
-    // Bucket::attempt_create("bucket", &config)?;
-    // let a = Bucket::create_bucket(&config)?;
-    // println!("{:?}", a);
+fn main() -> Result<()> {
+    let config = Config::new("us-east1", "neon-circle-400322", None);
 
 
-    check_for_python()?;
 
+    // create_bq(&config)?;
+    // create_table("table1", "test_sasdf", &config)?;
+
+    create_pubsub_topic(&config)?;
 
     Ok(())
 }
 
 
-pub fn check_for_python() -> Result<(String, String)> {
-    //!  - Inner line doc
-    ///  - Outer line doc (exactly 3 slashes)
-    /// Output Result< python command, pip command >
 
-    let py_version: Option<String>;
-    let py3_version: Option<String>;
-    let pip_version: Option<String>;
-    let pip3_version: Option<String>;
+pub fn create_service_account() -> Result<()> {
 
-    let mut py_command;
-    let mut pip_command;
 
-    match Command::new("python").arg("--version").output() {
-        Ok(output) => py_version = Some(String::from_utf8(output.stdout)?),
-        Err(E) => py_version = None,
-    }
-    match Command::new("python3").arg("--version").output() {
-        Ok(output) => py3_version = Some(String::from_utf8(output.stdout)?),
-        Err(E) => py3_version = None,
-    }
-    match Command::new("pip").arg("--version").output() {
-        Ok(output) => pip_version = Some(String::from_utf8(output.stdout)?),
-        Err(E) => pip_version = None,
-    }
-    match Command::new("pip3").arg("--version").output() {
-        Ok(output) => pip3_version = Some(String::from_utf8(output.stdout)?),
-        Err(E) => pip3_version = None,
-    }
 
-    if py_version.is_none() && py3_version.is_none() { panic!("Please have python installed on your system as `python` or `python3`"); }
-    if pip_version.is_none() && pip3_version.is_none() { panic!("Please have pip installed on your system as `pip` or `pip3`"); }
-
-    if py3_version.is_some() { py_command = "python3".to_string(); }
-    else { py_command = "python".to_string()}
-
-    if pip3_version.is_some() { pip_command = "pip3".to_string(); }
-    else { pip_command = "pip".to_string()}
-
-    Ok((py_command, pip_command))
+    Ok(())
 }
