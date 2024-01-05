dddf1cb15541c8f9568f04b39efe70ab55b52c36
Divkov575 <dmitriy@ivkov.net>
2024-01-05 11:54:41 -0500

    added role_updating and sercice_acc_impersonation

--- infra/src/service_accounts.rs
+++ infra/src/service_accounts.rs
@@ -1,6 +1,7 @@
 use std::fmt::format;
 use std::process::Command;
 use anyhow::Result;
+use crate::config::Config;
 
 pub fn create_service_account(sa_name: &str) -> Result<()> {
     let args: Vec<&str> =  Vec::from([
@@ -12,17 +13,32 @@ pub fn create_service_account(sa_name: &str) -> Result<()> {
     Ok(())
 }
 
-
-
-pub fn update_service_account_roles(sa_name: &str) -> Result<()> {
+pub fn update_service_account_roles(sa_name: &str, config: &Config, roles: Vec<String>) -> Result<()> {
     // gcloud projects add-iam-policy-binding PROJECT_ID \
     // --member="serviceAccount:SA_NAME@PROJECT_ID.iam.gserviceaccount.com" \
     // --role="ROLE_NAME"
+    let member_binding = format!("--member=serviceAccount:{}@{}.iam.gserviceaccount.com", sa_name, config.project);
+    let mut args: Vec<&str> =  Vec::from([
+        "projects", "add-iam-policy-binding", config.project,
+        &member_binding,
+    ]);
+    for role in roles.map(|x| format!("--role={}", x)) {
+        args.append(role)
+    }
+    Command::new("gcloud").args(args).status()?;
+    Ok(())
+}
+
+pub fn allow_service_account_impersonation(sa_name: &str, user_email: &str, config: &Config) -> Result<()> {
+    let sa_binding = format!("{}@{}.iam.gserviceaccount.com", sa_name, config.project);
+    let member_binding = format!("--member=user:{}", user_email);
     let args: Vec<&str> =  Vec::from([
-        "iam", "service-accounts", "create", sa_name,
-        "--description=\"Service account created by beaver SIEM tool for impersonation during resource creation\"",
-        "--display-name=\" Beaver Impersonation Account\""
+        "iam", "service-accounts", "add-iam-policy-binding",
+        &sa_binding,
+        &member_binding,
+        "--role=roles/iam.serviceAccountUser"
     ]);
     Command::new("gcloud").args(args).status()?;
+
     Ok(())
 }
