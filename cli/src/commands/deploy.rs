6e3d77aae9ad3ffc6a64a5ade5c2f65935b8e9d0
Divkov575 <dmitriy@ivkov.net>
2024-01-05 22:44:42 -0500

    refactoring structs to string, and added cell/refcell

--- cli/src/commands/deploy.rs
+++ cli/src/commands/deploy.rs
@@ -1,3 +1,4 @@
+use std::cell::{Cell, RefCell};
 use std::fs::{File, OpenOptions};
 use std::path::Path;
 
@@ -12,6 +13,8 @@ use crate::lib::{
     },
     resources
 };
+use crate::lib::pubsub::PubSub;
+use crate::lib::resources::Resources;
 
 pub fn deploy(path_arg: &str) -> Result<()> {
     let path = Path::new(path_arg);
@@ -21,30 +24,23 @@ pub fn deploy(path_arg: &str) -> Result<()> {
     let project: String = beaver_config[&Value::String("project".into())].clone().as_str().unwrap().to_owned();
     let config: Config = Config::new(&region, &project, None);
 
+    let mut resources: Resources = Resources::empty();
+    resources.biq_query = Some(RefCell::new(BqTable::new(config.project, "beaver_data_warehouse", "table1")));
+    resources.output_pubsub = Some(Cell::new(PubSub::empty()));
+
     bq::check_for_bq()?;
-    let bq_table = BqTable::new(config.project, "beaver_data_warehouse", "table1");
+    bq::create_dataset(&resources, &config)?;
+    bq::create_table(&resources, &config)?;
+    // let topic_id = pubsub::create_pubsub_topic(&config)?;
+    //
+    // pubsub::create_bq_subscription(&topic_id, &BqTable::new(config.project, "beaver_data_warehouse", "table1"), &config)?;
 
-    bq::create_dataset(bq_table.dataset_id, &config)?;
-    bq::create_table(bq_table.dataset_id, bq_table.table_id, &config)?;
-    let topic_id = pubsub::create_pubsub_topic(&config)?;
-
-    pubsub::create_bq_subscription(&topic_id, &bq_table, &config)?;
-
-
-    generate_vector_config(&path)?;
+    // generate_vector_config(&path)?;
 
 
 
     Ok(())
 }
-
-// fn read_config(path: &Path) -> Result<Config> {
-//    let beaver_config: Mapping = serde_yaml::from_reader(File::open(path.join("beaver_config.yaml"))?)?;
-//
-//     let region: String = beaver_config[&Value::String("region".into())].clone().as_str().unwrap().to_owned();
-//     let project: String = beaver_config[&Value::String("project".into())].clone().as_str().unwrap().to_owned();
-//     Ok(Config::new(&region, &project, None))
-// }
 
 fn generate_vector_config(path: &Path, resources: &Resources) -> Result<()> {
     let beaver_config_file = File::open(path.join("beaver_config.yaml"))?;
@@ -60,8 +56,6 @@ fn generate_vector_config(path: &Path, resources: &Resources) -> Result<()> {
     let transforms=  beaver_config[&Value::String("transforms".into())].clone();
 
     println!("{:?}", transforms);
-
-
 
     // let vector_config: Mapping = Mapping::from_iter([
     //     ("sources".into(), beaver_config[&Value::String("sources".into())].clone()),
--- cli/src/lib/resources.rs
+++ cli/src/lib/resources.rs
@@ -1,16 +1,18 @@
+use std::cell::{Cell, Ref, RefCell};
 use crate::lib::bq::BqTable;
+use crate::lib::pubsub::PubSub;
 
-pub struct Resources<'a> {
-    pub biq_query: Option<BqTable<'a>>,
-    pub gcs_bucket: Vec<String>,
-    pub output_pubsub: Option<String>
+pub struct Resources {
+    pub biq_query: Option<RefCell<BqTable>>,
+    pub gcs_bucket: Cell<Vec<String>>,
+    pub output_pubsub: Option<Cell<PubSub>>
 }
 
 impl Resources {
     pub fn empty() -> Self {
         Self {
             biq_query: None,
-            gcs_bucket: Vec::new(),
+            gcs_bucket: Cell::new(Vec::new()),
             output_pubsub: None,
         }
     }
