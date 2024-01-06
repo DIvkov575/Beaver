129520e58eada08309a18f2ed229dc8ff5c1b6bf
Divkov575 <dmitriy@ivkov.net>
2024-01-05 23:03:57 -0500

    addded pubsub_bq_write method, incomplete resouce managemnet

--- cli/src/lib/pubsub.rs
+++ cli/src/lib/pubsub.rs
@@ -1,3 +1,4 @@
+use std::cmp::Reverse;
 use std::fmt::Display;
 use std::process::Command;
 use crate::lib::config::Config;
@@ -6,18 +7,19 @@ use log::warn;
 use rand::distributions::Alphanumeric;
 use rand::Rng;
 use crate::lib::bq::BqTable;
+use crate::lib::resources::Resources;
 
 
 pub struct PubSub {
     topic_id: String,
-    subscriptions: Vec<String>
+    subscription_id: String
 }
 
 impl PubSub {
     pub fn empty() -> Self {
         Self {
             topic_id: String::new(),
-            subscriptions: Vec::new(),
+            subscription_id: String::new(),
         }
     }
 }
@@ -88,4 +90,19 @@ pub fn create_pubsub_topic(config: &Config) -> Result<String> {
         }
     }
    Ok(topic_binding)
+}
+
+
+pub fn create_pubsub_to_bq_subscription(resources: &Resources, config: &Config) -> Result<()> {
+    let bq_table = resources.biq_query.as_ref().unwrap().borrow();
+    let pubsub_binding = resources.output_pubsub.unwrap();
+    let pubsub = pubsub_binding.get_mut();
+
+    let topic_id = create_pubsub_topic(&config)?;
+    let subscription_id = create_bq_subscription(&topic_id, &bq_table, &config)?;
+
+    pubsub.topic_id = topic_id;
+    pubsub.subscription_id = subscription_id;
+
+    Ok(())
 }
