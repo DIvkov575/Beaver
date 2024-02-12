38749ea823d3e30fe02424b5a8c8e113ebf35ed8
Divkov575 <dmitriy@ivkov.net>
2024-02-11 23:00:23 -0500

    working dataflow generate_ast

--- src/lib/dataflow.rs
+++ src/lib/dataflow.rs
@@ -1,11 +1,12 @@
 use std::fs::File;
 use std::io::read_to_string;
 use std::path::Path;
+use std::ptr::write;
 use std::sync::WaitTimeoutResult;
 use anyhow::Result;
 use log::debug;
 use python_parser::ast;
-use python_parser::ast::CompoundStatement;
+use python_parser::ast::{CompoundStatement, Statement};
 use python_parser::ast::CompoundStatement::Funcdef;
 use python_parser::ast::Statement::Compound;
 use thiserror::Error;
@@ -25,24 +26,6 @@ pub fn create_classic_template() -> Result<(), anyhow::Error> {
 }
 
 
-macro_rules! get_function_ast {
-    ($ast: ident) => {
-        $ast
-            .iter()
-            .filter(|a| match a {
-                Compound(b) => match **b {
-                    Funcdef(c) => match c {
-                        ast::Funcdef{name, ..} => name == "detect",
-                        _ => false,
-                    },
-                    _ => false,
-                },
-                _ => false
-            })
-            .collect::<Vec<_>>()[0];
-    };
-}
-
 pub fn build_detection_file(config_path: &Path) -> Result<()> {
     let path = config_path.join("detections");
 
@@ -52,38 +35,53 @@ pub fn build_detection_file(config_path: &Path) -> Result<()> {
         if !file.as_path().exists() {
             return Err(DataflowError::MissingDetectFile(dir.path().to_str().unwrap().to_string()).into());
         } else {
-            let contents = read_to_string(File::open(&file.as_path())?)?;
-            let mut ast = python_parser::file_input(python_parser::make_strspan(&contents)).unwrap().1;
-            // let mut func_ast = ast
-            //     .iter()
-            //     .filter(|a| match a {
-            //         Compound(b) => match **b {
-            //             Funcdef(c) => match c {
-            //                 ast::Funcdef { name, .. } => name == "detect",
-            //             },
-            //             _ => false,
-            //         },
-            //         _ => false
-            //     })
-            //     .collect::<Vec<_>>();
-            // if func_ast.len() != 1 { return Err(DataflowError::MissingDetectFunction(file.as_path().to_str().unwrap().to_string()).into()); }
+            let input_ast = generate_ast(&file)?;
+            let output_ast = python_parser::visitors::printer::format_module(&input_ast);
 
-            if let Compound(a) = ast {
-                if let Funcdef(b) = *a {
-                    if let ast::Funcdef {name, .. } = b {
-                        if name != "detect" {
-                            return Err(DataflowError::MissingDetectFunction(file.as_path().to_str().unwrap().to_string()).into());
-                        } else {
-                            name =
-                        }
-                    }
-                }
-            }
+
         }
     }
 
 
     Ok(())
+}
+
+
+fn generate_ast(file_path: &Path) -> Result<Vec<Statement>> {
+    let name = file_path.file_name().unwrap().to_str().unwrap().to_string();
+    let contents = read_to_string(File::open(&file_path)?)?;
+    let mut input_ast = python_parser::file_input(python_parser::make_strspan(&contents)).unwrap().1;
+
+    for statement in input_ast {
+        if let Compound(a) = statement {
+            if let Funcdef(b) = *a {
+                if let ast::Funcdef {name, code, .. } = b {
+
+                    if name != "detect" {
+                        return Err(DataflowError::MissingDetectFunction(file_path.to_str().unwrap().to_string()).into());
+                    } else {
+                        return Ok(Vec::from_iter([
+                            Compound(Box::new(
+                                Funcdef(
+                                    ast::Funcdef {
+                                        r#async: false,
+                                        decorators: vec![],
+                                        name,
+                                        parameters: Default::default(),
+                                        return_type: None,
+                                        code,
+                                    }
+                                )
+                            ))
+                        ]));
+                    }
+
+                }
+            }
+        }
+    }
+
+    return Err(DataflowError::MiscASTGen.into())
 }
 
 
@@ -94,5 +92,8 @@ enum DataflowError {
     MissingDetectFile(String),
     #[error("Detect.py file is missing a function named detect")]
     MissingDetectFunction(String),
+    #[error("Error occurred while generating AST")]
+    MiscASTGen
+
 }
 
