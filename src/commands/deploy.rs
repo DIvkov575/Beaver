23b793a0839a980a5198cb1a531c547637fbcad1
Divkov575 <dmitriy@ivkov.net>
2024-02-13 20:57:39 -0500

    (broken) (commented out all) added ast insert + added doc link / eg of func call

--- src/commands/deploy.rs
+++ src/commands/deploy.rs
@@ -23,7 +23,7 @@ pub fn deploy(path_arg: &str) -> Result<()> {
     // let mut resources: Resources =  serde_yaml::from_reader(resources_file)?;
 
     // sigma::generate_detections(&path)?;
-    dataflow::get_detection_funcs(&path)?;
+    dataflow::generate_detections_file(&path)?;
 
 
     // bq::create(&resources, &config)?;
